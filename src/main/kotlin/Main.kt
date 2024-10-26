e8e4a8306e22c54ece43dbaa011bf4956e3ab35c
Serrgey Litovchenko <e4sybreezy@gmail.com>
2024-10-26 18:24:28 +0600

    commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,5 @@
 package org.example
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
     val likes = 41
     val word = if (likes % 10 == 1 && likes != 11) "человеку" else "людям"
